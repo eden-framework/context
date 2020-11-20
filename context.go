5c41ef5d4b997c39d9b5fd0f62dea531ddebb525
liyiwen <johnnyeven@gmail.com>
2020-11-20 10:41:39 +0800

    init

--- context.go
+++ context.go
@@ -35,3 +35,23 @@ func (c *WaitStopContext) Finish() {
 func (c *WaitStopContext) Done() <-chan struct{} {
 	return c.ctx.Done()
 }
+
+func (c *WaitStopContext) Value(key interface{}) interface{} {
+	return c.ctx.Value(key)
+}
+
+func WithValue(parent *WaitStopContext, key interface{}, value interface{}) {
+	parent.ctx = context.WithValue(parent.ctx, key, value)
+}
+
+const (
+	TempCancelContextKey = "temp_cancel_func"
+)
+
+func WithTempCancel(parent *WaitStopContext, cancelFunc context.CancelFunc) {
+	parent.ctx = context.WithValue(parent.ctx, TempCancelContextKey, cancelFunc)
+}
+
+func (c *WaitStopContext) TempCancel() context.CancelFunc {
+	return c.ctx.Value(TempCancelContextKey).(context.CancelFunc)
+}
